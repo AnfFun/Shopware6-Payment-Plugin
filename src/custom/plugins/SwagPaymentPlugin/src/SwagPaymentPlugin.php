f08372031906aca29441afb5cb9465263e96b54d
max <prokofmaxwork@gmail.com>
2023-09-07 10:00:22 +0300

    change .gitignore and create plugin

---
+++ src/custom/plugins/SwagPaymentPlugin/src/SwagPaymentPlugin.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace SwagPaymentPlugin;
+
+use Shopware\Core\Framework\Plugin;
+
+class SwagPaymentPlugin extends Plugin
+{
+}
---
+++ src/custom/plugins/SwagPaymentPlugin/tests/TestBootstrap.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use Shopware\Core\TestBootstrapper;
+
+$loader = (new TestBootstrapper())
+    ->addCallingPlugin()
+    ->addActivePlugins('SwagPaymentPlugin')
+    ->bootstrap()
+    ->getClassLoader();
+
+$loader->addPsr4('SwagPaymentPlugin\\Tests\\', __DIR__);
