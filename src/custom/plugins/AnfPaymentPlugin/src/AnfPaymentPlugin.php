b6817fbc81d4153270cf32c178f69abb03809a9a
max <prokofmaxwork@gmail.com>
2023-09-08 11:41:58 +0300

    push extension

---
+++ src/custom/plugins/AnfPaymentPlugin/src/AnfPaymentPlugin.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Anf\PaymentPlugin;
+
+use Shopware\Core\Framework\Plugin;
+
+class AnfPaymentPlugin extends Plugin
+{
+}
---
+++ src/custom/plugins/AnfPaymentPlugin/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'shopware-platform-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '276357c74980ad66ad95f2e757ab7b277b8afd61',
+        'name' => 'anf/paymentplugin',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'anf/paymentplugin' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'shopware-platform-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '276357c74980ad66ad95f2e757ab7b277b8afd61',
+            'dev_requirement' => false,
+        ),
+    ),
+);
