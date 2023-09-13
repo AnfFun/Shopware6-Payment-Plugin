497fc8bbce7ae9361128d64b288e0def74baa524
AnfFun <prokofmaxwork@gmail.com>
2023-09-13 17:06:48 +0300

    Fix incorrect namespace issue

--- src/Service/AnfPaymentHandler.php
+++ src/Service/AnfPaymentHandler.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Anf\Payment\Service;
+namespace Anf\PaymentPlugin\Service;
 
 use Shopware\Core\Checkout\Payment\PaymentException;
 use Shopware\Core\Checkout\Payment\Cart\AsyncPaymentTransactionStruct;
