398da2a68400ec5d577f70df708e16e5f0782aac
AnfFun <prokofmaxwork@gmail.com>
2023-09-13 16:44:08 +0300

    Create handler for payment method and complete correct services.xml && Create install-uninstall method for plugin

--- src/AnfPaymentPlugin.php
+++ src/AnfPaymentPlugin.php
@@ -2,24 +2,100 @@
 
 namespace Anf\PaymentPlugin;
 
-use Shopware\Core\Checkout\Payment\Cart\AsyncPaymentTransactionStruct;
-use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\AsynchronousPaymentHandlerInterface;
-use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\PaymentHandlerInterface;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Plugin;
-use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
-use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
+use Shopware\Core\Framework\Plugin\Context\ActivateContext;
+use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Core\Framework\Plugin\Util\PluginIdProvider;
+use Anf\Payment\Service\AnfPaymentHandler;
 
-class AnfPaymentPlugin extends Plugin implements AsynchronousPaymentHandlerInterface
+class AnfPaymentPlugin extends Plugin
 {
-    public function pay(AsyncPaymentTransactionStruct $transaction, RequestDataBag $dataBag, SalesChannelContext $salesChannelContext): RedirectResponse
+    public function install(InstallContext $context): void
     {
-        // TODO: Implement pay() method.
-    }
-    public function finalize(AsyncPaymentTransactionStruct $transaction, Request $request, SalesChannelContext $salesChannelContext): void
-    {
-        // TODO: Implement finalize() method.
+        $this->addPaymentMethod($context->getContext());
     }
 
+    public function uninstall(UninstallContext $context): void
+    {
+        // Only set the payment method to inactive when uninstalling. Removing the payment method would
+        // cause data consistency issues, since the payment method might have been used in several orders
+        $this->setPaymentMethodIsActive(false, $context->getContext());
+    }
+
+    public function activate(ActivateContext $context): void
+    {
+        $this->setPaymentMethodIsActive(true, $context->getContext());
+        parent::activate($context);
+    }
+
+    public function deactivate(DeactivateContext $context): void
+    {
+        $this->setPaymentMethodIsActive(false, $context->getContext());
+        parent::deactivate($context);
+    }
+
+    private function addPaymentMethod(Context $context): void
+    {
+        $paymentMethodExists = $this->getPaymentMethodId();
+
+        // Payment method exists already, no need to continue here
+        if ($paymentMethodExists) {
+            return;
+        }
+
+        /** @var PluginIdProvider $pluginIdProvider */
+        $pluginIdProvider = $this->container->get(PluginIdProvider::class);
+        $pluginId = $pluginIdProvider->getPluginIdByBaseClass(get_class($this), $context);
+
+        $PaymentData = [
+            // payment handler will be selected by the identifier
+            'handlerIdentifier' => AnfPaymentHandler::class,
+            'name' => 'PayPlug',
+            'description' => 'Custom payment method for shopware',
+            'pluginId' => $pluginId,
+            // if true, payment method will also be available after the order 
+            // is created, e.g. if payment fails and the user wants to try again
+            'afterOrderEnabled' => true,
+        ];
+
+        /** @var EntityRepository $paymentRepository */
+        $paymentRepository = $this->container->get('payment_method.repository');
+        $paymentRepository->create([$PaymentData], $context);
+    }
+
+    private function setPaymentMethodIsActive(bool $active, Context $context): void
+    {
+        /** @var EntityRepository $paymentRepository */
+        $paymentRepository = $this->container->get('payment_method.repository');
+
+        $paymentMethodId = $this->getPaymentMethodId();
+
+        // Payment does not even exist, so nothing to (de-)activate here
+        if (!$paymentMethodId) {
+            return;
+        }
+
+        $paymentMethod = [
+            'id' => $paymentMethodId,
+            'active' => $active,
+        ];
+
+        $paymentRepository->update([$paymentMethod], $context);
+    }
+
+    private function getPaymentMethodId(): ?string
+    {
+        /** @var EntityRepository $paymentRepository */
+        $paymentRepository = $this->container->get('payment_method.repository');
+
+        // Fetch ID for update
+        $paymentCriteria = (new Criteria())->addFilter(new EqualsFilter('handlerIdentifier', AnfPaymentHandler::class));
+        return $paymentRepository->searchIds($paymentCriteria, Context::createDefaultContext())->firstId();
+    }
 }
